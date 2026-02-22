c853adc1cf5ec9251b6d5fae2eb0ca57b21831ba
tech1savvy <amankumar010604@gmail.com>
2026-02-22 18:11:15 +0530

    feat(terraform): add nixos-node module for creating NixOS EC2 instances
    - data resource searches for the latest NixOS AMI based on the provided architecture

---
+++ terraform/modules/nixos-node/variables.tf
@@ -0,0 +1,43 @@
+variable "vpc_id" {
+  description = "The VPC ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID"
+  type        = string
+}
+
+variable "node_name" {
+  description = "The name for the node"
+  type        = string
+}
+
+variable "public_key" {
+  description = "The public key for SSH access"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "architecture" {
+  description = "The CPU architecture (x86_64 or arm64)"
+  type        = string
+  default     = "x86_64"
+}
+
+variable "instance_state" {
+  description = "The desired state of the instance (running or stopped)"
+  type        = string
+  default     = "running"
+}
+
+variable "root_volume_size" {
+  description = "The size of the root volume in GB"
+  type        = number
+  default     = 20
+}
