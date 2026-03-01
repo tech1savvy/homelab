ec0e4cc6381e676253165dfc90bd6d93c634e251
tech1savvy <amankumar010604@gmail.com>
2026-03-01 18:29:07 +0530

    feat: set hostname to lab and domain to tech1savvy.me, also allow tcp ports 80 and 443

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,10 @@
 }:
 {
   services.openssh.enable = true;
+  networking = {
+    hostName = "lab";
+    domain = "tech1savvy.me";
+  };
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.hello
@@ -12,6 +16,11 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPY5PE/6LzFDNE6omKBo+nfl/XWAqeq3GcF/3Er1kxMY"
+  ];
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
   ];
 
   system.stateVersion = "25.11";
