18f20377b152c112e18bdd4da48bca1d11d6a7db
tech1savvy <amankumar010604@gmail.com>
2026-03-13 18:19:10 +0530

    feat(terraform): add s3 backend for state storage

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,23 @@
-provider "aws" {
-  region = var.region
+ephemeral "sops_file" "secrets" {
+  source_file = "secrets.terraform.json"
 }
+data "sops_file" "identifiers" {
+  source_file = "identifiers.terraform.json"
+}
+
+locals {
+  secrets     = jsondecode(ephemeral.sops_file.secrets.raw)
+  identifiers = jsondecode(data.sops_file.identifiers.raw)
+}
+
+provider "aws" {
+  region = local.identifiers.region
+}
+
+provider "cloudflare" {
+  api_token = local.secrets.cloudflare_api_token
+}
+
 
 terraform {
   required_version = ">= 1.5.7"
@@ -9,5 +26,22 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 6.28"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "tech1savvy-terraform-state"
+    key          = "lab/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
   }
 }
+
+
