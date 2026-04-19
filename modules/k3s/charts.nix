f9b3a0ab2a21fa6eaaf09847cb4ddcfee8dda8ea
tech1savvy <amankumar010604@gmail.com>
2026-04-19 22:52:37 +0530

    fix: disable kube-prometheus-stack and change architecture to x86_64

--- modules/k3s/charts.nix
+++ modules/k3s/charts.nix
@@ -31,7 +31,7 @@
       };
       # Monitoring
       "kube-prometheus-stack" = {
-        enable = true;
+        enable = false;
         name = "kube-prometheus-stack";
         repo = "https://prometheus-community.github.io/helm-charts";
         version = "82.4.3";
